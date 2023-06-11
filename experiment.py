75324da6a58f01212e96af1a2cb14b39f81cb70e
Robert Stok <robert.stok20@imperial.ac.uk>
2023-06-11 15:22:03 +0100

    [feat] created new small PFRNN model

--- experiment.py
+++ experiment.py
@@ -1,6 +1,7 @@
 from stochastic_volatility import SVL1, SVL1Paramters, HarveySV, HarveySVParamters
 from rob import SVMParamterEstimator, ModelArgs
 from harvey_sv_model import HarveySVPF
+from small_pfrnn_model import SmallPFRNNModel
 from lstm_model import LSTM1
 
 import os
@@ -32,18 +33,18 @@ if __name__=="__main__":
 
     # training config
     config = {
-        "samples": 1000,
+        "samples": 500,
         "sequence_length": 300,
         "window_size": 1,
         "train_test_split": 0.9,
-        "epochs": 2, # set to 0 if you don't want to train the model
-        "batch_size": 10,
+        "epochs": 3, # set to 0 if you don't want to train the model
+        "batch_size": 25,
         "learning_rate": 0.0005,
-        "load_model_from_previous": True,
+        "load_model_from_previous": False,
         "load_data_from_previous": False,
         "save_models": True,
-        "base_path": "./models/harvey_pfrnn_second_training",
-        "model_path": "./models/pfrnn_epoch_1.pt",
+        "base_path": "./models/small_pfrnn",
+        "model_path": "./models/small_pfrnn_0.pt",
         "use_gpu": False
     }
 
@@ -170,7 +171,7 @@ if __name__=="__main__":
     # 
     # we also create an optimizer
 
-    model = HarveySVPF(model_config)
+    model = SmallPFRNNModel(model_config)  
     #model = SVMParamterEstimator(model_config)
     #model = LSTM1(1, config["window_size"], 150, 1)
     if torch.cuda.is_available() and config["use_gpu"]:
--- rob_pfrnns.py
+++ rob_pfrnns.py
@@ -149,3 +149,119 @@ class PFRNN(nn.Module):
         h1, p1 = self.resampling(h1, p1)
 
         return h1, p1
+    
+
+class SmallPFRNN(nn.Module):
+    def __init__(self, num_particles, input_size=1, hidden_size=1, ext_obs=1, resamp_alpha=0.1):
+        """
+        :param num_particles: number of particles for a PF-RNN
+        :param input_size: the size of input x_t
+        :param hidden_size: the size of the hidden particle h_t^i
+        :param ext_obs: the size for o_t(x_t)
+        :param ext_act: the size for u_t(x_t)
+        :param resamp_alpha: the control parameter \alpha for soft-resampling.
+        We use the importance sampling with a proposal distribution q(i) = \alpha w_t^i + (1 - \alpha) (1 / K)
+        """
+        super(SmallPFRNN, self).__init__()
+        self.num_particles = num_particles
+        self.input_size = input_size
+        self.h_dim = hidden_size
+        self.ext_obs = ext_obs
+        self.resamp_alpha = resamp_alpha
+        self.train_obs_model = True
+        self.train_trans_model = True
+
+        # self.fc_obs = nn.Linear(self.ext_obs + self.h_dim, 1)
+        self.fc_obs_l1 = nn.Linear(self.ext_obs + self.h_dim, 20)
+        self.fc_obs_l2 = nn.Linear(20, 1)
+        self.fc_obs = nn.Sequential(
+            self.fc_obs_l1,
+            nn.Sigmoid(),
+            self.fc_obs_l2,
+            nn.Sigmoid(),
+        )
+
+        self.batch_norm = nn.BatchNorm1d(self.num_particles)
+
+        # init the layers for our transition function
+        # this will take as input a hidden state (volatility) and normaly dist. random float
+        # TODO: let it take parameter values as input
+
+        self.fc_trans_l1 = nn.Linear(self.h_dim, 20)
+        self.fc_trans_l2 = nn.Linear(20, 1)
+        self.fc_trans = nn.Sequential(
+            self.fc_trans_l1,
+            nn.Sigmoid(),
+            self.fc_trans_l2,
+        )
+
+        # here we define a simple layer to determine the variance to use for reparameterization
+        self.fc_var_l1 = nn.Linear(self.h_dim + 1, 1)
+        self.fc_var = nn.Sequential(
+            self.fc_var_l1,
+            nn.Sigmoid(),
+        )
+
+    def resampling(self, particles, prob):
+        """
+        The implementation of soft-resampling. We implement soft-resampling in a batch-manner.
+
+        :param particles: \{(h_t^i, c_t^i)\}_{i=1}^K for PF-LSTM and \{h_t^i\}_{i=1}^K for PF-GRU.
+                        each tensor has a shape: [num_particles * batch_size, h_dim]
+        :param prob: weights for particles in the log space. Each tensor has a shape: [num_particles * batch_size, 1]
+        :return: resampled particles and weights according to soft-resampling scheme.
+        """
+        resamp_prob = self.resamp_alpha * prob + (1 - self.resamp_alpha) * 1 / self.num_particles
+        resamp_prob = resamp_prob.view(self.num_particles, -1)
+        indices = torch.multinomial(resamp_prob.transpose(0, 1),
+                                    num_samples=self.num_particles, replacement=True)
+        batch_size = indices.size(0)
+        indices = indices.transpose(1, 0).contiguous()
+        offset = torch.arange(batch_size).type(torch.LongTensor).unsqueeze(0)
+        if torch.cuda.is_available():
+            offset = offset.cuda()
+        indices = offset + indices * batch_size
+        flatten_indices = indices.view(-1, 1).squeeze()
+
+        # PFLSTM
+        if type(particles) == tuple:
+            particles_new = (particles[0][flatten_indices],
+                             particles[1][flatten_indices])
+        # PFGRU
+        else:
+            particles_new = particles[flatten_indices]
+
+        prob_new = torch.exp(prob.view(-1, 1)[flatten_indices])
+        prob_new = prob_new / (self.resamp_alpha * prob_new + (1 -
+                                                               self.resamp_alpha) / self.num_particles)
+        prob_new = prob_new.view(self.num_particles, -1, 1)
+        prob_new = prob_new / torch.sum(prob_new, dim=0, keepdim=True)
+        prob_new = prob_new.view(-1, 1)
+
+        return particles_new, prob_new
+
+
+    def forward(self, input_, hx):
+        h0, p0 = hx
+        batch_size = h0.size(0)
+
+        h1 = self.fc_trans(h0)
+
+        # add noise
+        var = self.fc_var(torch.concat((h0, input_), dim=1))
+        std = torch.nn.functional.softplus(var)
+        if torch.cuda.is_available():
+            eps = torch.cuda.FloatTensor(std.shape).normal_()
+        else:
+            eps = torch.FloatTensor(std.shape).normal_()
+        h1 = h1 + eps
+        
+        obs_liklihood = self.fc_obs(torch.concat((h1, input_), dim=1))
+        p1 = obs_liklihood.view(self.num_particles, -1, 1) * \
+            p0.view(self.num_particles, -1, 1)
+
+        p1 = p1 / torch.sum(p1, dim=0, keepdim=True)
+
+        h1, p1 = self.resampling(h1, p1)
+
+        return h1, p1
